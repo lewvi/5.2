98bccb58571dbe975c684733e76cc1b53e10a538
lewvi <lew_rip@hotmail.com>
2020-07-31 13:51:10 +0700

    Add a game-finished event

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -90,7 +90,7 @@ class GameFragment : Fragment() {
 
     private fun gameFinished() {
         Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
-        
+
         val action = GameFragmentDirections.actionGameToScore()
         action.score = viewModel.score.value ?: 0
        NavHostFragment.findNavController(this).navigate(action)
